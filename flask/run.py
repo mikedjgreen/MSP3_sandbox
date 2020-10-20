07ba8fa161185b58fba606faae14288a648cb1fb
Mike Green <m.d.j.green@ntlworld.com>
2020-10-20 13:39:39 +0000

    Routing HTML members membership

--- flask/run.py
+++ flask/run.py
@@ -5,9 +5,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
+@app.route("/members")
+def members():
     return render_template("members.html")
+
+
+@app.route("/membership")
+def membership():
+    return render_template("membership.html")
 
 
 if __name__ == '__main__':
