9a08214e9f4f95e6013c5f82b23955cd25e3c1da
Mike Green <m.d.j.green@ntlworld.com>
2020-10-21 08:16:01 +0000

    Flash added to membership form

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import os
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 
 
 app = Flask(__name__)
+app.secret_key = 'prickwillow'
 
 
 @app.route("/")
@@ -17,7 +18,9 @@ def members():
 @app.route("/membership",methods=["GET","POST"])
 def membership():
     if request.method == "POST":
-        print(request.form)
+        flash("Thanks {}, we have received your message".format(
+            request.form["name"]
+        ))
     return render_template("membership.html",page_title="Membership form")
 
 
